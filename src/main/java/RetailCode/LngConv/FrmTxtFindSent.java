94357a1bc14a840d4a1e716662acec8b08910126
Cevdet-Efe <cevdet71.efe@gmail.com>
2022-11-23 01:42:59 -0500

    Language Covert Code Update

--- src/main/java/RetailCode/LngConv/FrmTxtFindSent.java
+++ src/main/java/RetailCode/LngConv/FrmTxtFindSent.java
@@ -1,8 +1,17 @@
 package RetailCode.LngConv;
+import java.sql.*;
 
 public class FrmTxtFindSent {
-    public static void main(String[] args) {
-        System.out.println("I started My project");
+    public static void main(String[] args) throws SQLException {
+        String connectionURL = "jdbc:oracle:thin:Win10SDbase:RETAILDB";
+        String userName = "MAIN";
+        String password = "MAIN";
+        Connection connection = DriverManager.getConnection(connectionURL,userName,password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT EMTIANO,EMTIAAD FROM MASK0001");
+        while(resultSet.next()){
+            System.out.println("Emtiano :"+resultSet.getString(1));
+        }
     }
 
 }
